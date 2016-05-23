d25bd9371ae7185ed37216639aa733c68799f3bd
MykolaRohoza <rohoza_mykola@rambler.ru>
2016-05-23 15:28:45 +0300

    Create view_base.php

---
+++ view/view_base.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>   
+        <title>ОПН</title>
+        <link  type="text/css" rel="stylesheet" href="view/css/style.css">
+        <script src="js/jquery-2.1.1.min.js"></script>
+        <script src="/js/main.js"></script>
+    </head>
+    <body>
+        <header>
+        </header>
+        <main>
+            <div id="container">
+                <?=$container?>
+            </div>
+                
+        </main>
+    </body>
+    
+</html> 
