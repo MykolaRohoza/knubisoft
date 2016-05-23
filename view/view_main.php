0a19016f44dd1dd433d44fb7ce29e0b9077ef19d
MykolaRohoza <rohoza_mykola@rambler.ru>
2016-05-23 15:29:21 +0300

    Add files via upload

---
+++ view/view_main.php
@@ -0,0 +1,11 @@
+
+
+<div class="holder">
+    <div id="res">Введите пример в обратной пользовательской нотации "3 7 +"</div>   
+    <br>
+    <form method="post">                
+                <input name="not" type="text">
+                <input name="calc" type="button" value="Подсчитать">
+            </form>
+</div>
+
